cd420114a25944d5a5f2193f954a0ba5386ad442
Mohammed Rishalat <103600985+Rishu2140@users.noreply.github.com>
2024-01-27 17:25:19 +0530

    Add files via upload

---
+++ Guessing_Game.py
@@ -0,0 +1,22 @@
+import random 
+import math 
+
+lower = int(input("Enter the lower bound")) 
+upper = int(input("Enter the upper bound")) 
+
+print("You have only ",round(math.log(upper - lower + 1,2))," chances to guesss the numeber")
+number = random.randint(lower, upper) 
+count = 0
+while count < math.log(upper - lower + 1):
+    count+=1
+    x = int(input("Guess the Number"))
+
+    if x==number:
+        print("Yes the number is ",number," You did it in ",count," try")         
+        break
+    elif x > number:
+        print("You Choose to big number") 
+    elif x < number:
+        print("You guess too short number") 
+if count > math.log(upper - lower + 1):
+    print("Better luck next time")
